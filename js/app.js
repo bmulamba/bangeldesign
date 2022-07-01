128fa0f931846de98e449fa0d41869aacb6469b8
bmulamba <badibangamulamba@gmail.com>
2022-07-01 23:43:28 +0200

    updateSinglePage

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@ const bar = document.getElementById("bar");
 const close = document.getElementById("close");
 const nav = document.getElementById("navbar");
 
+const MainImg = document.getElementById("MainImg");
+const SmlImg = document.getElementsByClassName("small-img");
+
+
 bar.addEventListener('click', () => {
     nav.classList.add('active')
 });
@@ -10,5 +14,29 @@ bar.addEventListener('click', () => {
 close.addEventListener('click', () => {
     nav.classList.remove('active')
 });
+
+// select small imge 
+
+SmlImg[0].addEventListener('click', () =>{
+    MainImg.src = SmlImg[0].src;
+    // console.log(";lkkk");
+});
+
+SmlImg[1].addEventListener("click", () =>{
+    MainImg.src = SmlImg[1].src;
+});
+
+SmlImg[2].addEventListener("click", () =>{
+    MainImg.src = SmlImg[2].src;
+});
+
+SmlImg[3].addEventListener("click", () =>{
+    MainImg.src = SmlImg[3].src;
+});
+
+
+// single page 
+
+
 
 
